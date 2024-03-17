b72c88b16a50a7ff51b4016608a7efa8a9264944
lemuelsousa <lemuel124.ldsp@gmail.com>
2024-03-17 17:09:01 -0300

    feat: rename entity

--- src/main/kotlin/amedigital/challenge/backend/Model.kt
+++ src/main/kotlin/amedigital/challenge/backend/Model.kt
@@ -1,10 +1,6 @@
 package amedigital.challenge.backend
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity(name = "planets")
 data class Planet(
